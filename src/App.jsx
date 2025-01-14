35187394ef610256e68fccc019b13f1c77a22461
fridaSea <maria.kluge.codes@gmail.com>
2025-01-14 15:15:12 +0100

    Set the titel and favicon in browser tab

--- src/App.jsx
+++ src/App.jsx
@@ -2,6 +2,7 @@ import Footer from './Footer.jsx';
 //import Cards from './Cards.jsx'; 
 import DataFetch from './Fetch.jsx';
 //import CharacterGrid from './Fetch.jsx';
+import Navbar from './Navbar.jsx';
 
 //Font Import
 import '@fontsource/roboto/300.css';
@@ -9,10 +10,18 @@ import '@fontsource/roboto/400.css';
 import '@fontsource/roboto/500.css';
 import '@fontsource/roboto/700.css';
 
+// const useStyles = makeStyles({
+//   cardStyle:{
+//     margin: 10px 
+//   },
+
+// });
+
 
 function App() {
   return(
     <> 
+     <Navbar/>
     <DataFetch/>
     {/* <Cards/> */}
     {/* <CharacterGrid/> */}
